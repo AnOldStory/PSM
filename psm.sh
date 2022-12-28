5063aa4ab5d93a4a3a49037848abaee804bcd3af
AnOldStory <hc9904@naver.com>
2022-12-28 22:19:13 +0900

    Update IO save

--- psm.sh
+++ psm.sh
@@ -27,13 +27,14 @@ function .log () {
 ISBUILD=false
 ISTEST=false
 ISPRESERVE=false
-ISVERBOSE=0
+ISVERBOSE=3
 
 POSITIONAL_ARGS=()
 RELATIVE_DIR=""
 CANONICAL_DIR=""
 SOURCE_DIR=""
 TESTCASE_DIR=""
+TEMP_DIR=""
 
 
 function parseArg () {
@@ -43,16 +44,12 @@ function parseArg () {
             -b|--build|build)
                 # build source code
                 ISBUILD=true
-                QUESTION_NUMBER="$2"
-                shift # past argument
                 shift # past value
                 ;;
             -t|--test|test)
                 # test with testcase
                 ISBUILD=true
                 ISTEST=true
-                QUESTION_NUMBER="$2"
-                shift # past argument
                 shift # past value
                 ;;
             -s|--save|save)
@@ -72,8 +69,12 @@ function parseArg () {
                 .log 3 "Unknown option $1"
                 exit 1
                 ;;
+            ''|*[!0-9]*)
+                .log 3 "Question Number is required."
+                exit 1
+                ;;
             *)
-                POSITIONAL_ARGS+=("$1") # save positional argument
+                QUESTION_NUMBER="$1"
                 shift # past argument
                 ;;
         esac
@@ -88,8 +89,9 @@ function setDir() {
     CANONICAL_DIR=`readlink -f $RELATIVE_DIR`
     .log 7 'CANONICAL DIR:' $CANONICAL_DIR # absolute directory
 
-    SOURCE_DIR=$CANONICAL_DIR/archive
-    TESTCASE_DIR=$CANONICAL_DIR/testcase
+    SOURCE_DIR=$CANONICAL_DIR/archive/$QUESTION_NUMBER
+    TESTCASE_DIR=$CANONICAL_DIR/archive/$QUESTION_NUMBER
+    TEMP_DIR=$CANONICAL_DIR/temp
 }
 
 
@@ -98,7 +100,7 @@ function setDir() {
 ######
 
 function buildScript () {
-    .log 6 "Checking Question Number..."
+    .log 6 "Checking Question Number : $QUESTION_NUMBER"
     file=`find $SOURCE_DIR -name $QUESTION_NUMBER.*` # find file in archived
     if [[ -e $SOURCE_DIR/$file ]]; then # TODO: Why Working reverse??
         .log 3 "File not exists. [$file]"
@@ -108,10 +110,10 @@ function buildScript () {
     .log 6 "Build Start with [$file]"
     case ${file##*.} in ## check file extension && build
         c)
-            gcc $SOURCE_DIR/$QUESTION_NUMBER.c -o $CANONICAL_DIR/$QUESTION_NUMBER
+            gcc $file -o $TEMP_DIR/$QUESTION_NUMBER
         ;;
         cpp)
-            g++ $SOURCE_DIR/$QUESTION_NUMBER.cpp -o $CANONICAL_DIR/$QUESTION_NUMBER
+            g++ $file -o $TEMP_DIR/$QUESTION_NUMBER
         ;;
         *)
             .log 3 "Unsupported file extension."
@@ -129,7 +131,7 @@ function testAll () {
     for test in "$TESTCASE_DIR"/input/*
     do
         if [ -e $test ]; then 
-            cat $test | $CANONICAL_DIR/$QUESTION_NUMBER > $TESTCASE_DIR/output/$(basename $test)
+            cat $test | $TEMP_DIR/$QUESTION_NUMBER > $SOURCE_DIR/output/$(basename $test)
         fi 
     done        
 }
@@ -140,31 +142,36 @@ function testAll () {
 
 function before_clean () {
     .log 6 "Clear Before Temp File"
-    rm -rf $TESTCASE_DIR/output
-    mkdir $TESTCASE_DIR/output
+    rm -rf $SOURCE_DIR/output
+    mkdir $SOURCE_DIR/output
 }
 
 function after_clean () {
     .log 6 "Clear After Temp File"
-    rm -f $CANONICAL_DIR/$QUESTION_NUMBER
+    rm -f $TEMP_DIR/$QUESTION_NUMBER
 }
 
 function main() {
     parseArg $@
 
+    .log 7 "Set Directory"
     setDir
 
+    .log 7 "Clean before start"
     before_clean
 
     if [[ $ISBUILD == "true" ]]; then 
+        .log 7 "Build Start"
         buildScript     
     fi
 
     if [[ $ISTEST == "true" ]]; then
+        .log 7 "Test Start"
         testAll
     fi
 
     if [[ $ISPRESERVE == "false" ]]; then
+        .log 7 "Clean after Start"
         after_clean
     fi
 
